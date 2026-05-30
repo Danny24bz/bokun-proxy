bb31104f565baef97a43e3b00d97848c66d88f22
Danny24bz <darwinmcculloch@outlook.com>
2026-05-29 21:49:22 -0700

    Fix CORS headers to allow all origins including local files

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const https = require("https");
 const crypto = require("crypto");
 const app = express();
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  const origin = req.headers.origin || "*";
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+  res.header("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") return res.sendStatus(200);
   next();
 });
