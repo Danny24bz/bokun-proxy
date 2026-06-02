1ad97936f4ecda9d29d45a7f9d1ae6aba9361fc1
Danny24bz <darwinmcculloch@outlook.com>
2026-06-02 16:20:48 -0700

    Add combo field

--- server.js
+++ server.js
@@ -126,6 +126,7 @@ app.post("/push", async (req, res) => {
     startTimes: [{ hour: 9, minute: 0 }],
     meetingType: { type: "MEET_ON_LOCATION", meetingPointAddresses: [{ title: meetPt, address: { addressLine1: meetPt, city: "Belize City", countryCode: "BZ" } }], dropoffService: false },
     boxSettings: { isBox: false },
+    combo: { isCombo: false },
     activation: { activated: false },
     pricingCategories: { defaultId: 1153185, ids: [1153185, 1153187] },
     rates: {
