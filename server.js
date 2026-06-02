56872bf93499f1589ce1e06a13e294c6595c91b2
Danny24bz <darwinmcculloch@outlook.com>
2026-06-02 14:55:51 -0700

    Add allPricingCategories false alongside pricingCategoryIds

--- server.js
+++ server.js
@@ -136,7 +136,8 @@ app.post("/push", async (req, res) => {
         externalId: "default-rate",
         cancellationPolicyId: 258744,
         pricingCategoryIds: [1153185, 1153187],
-        pricedPerPerson: true
+        pricedPerPerson: true,
+        allPricingCategories: false
       }]
     },
     availabilityRules: [{ recurrenceRule: {}, allStartTimes: true, maxCapacity: 14 }]
